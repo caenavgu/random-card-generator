fc7a83166d0da5588c390c993e11f183ffb378ba
Carlos <caenavgu@gmail.com>
2017-11-23 01:00:38 +0000

    html and css ready

--- src/app.js
+++ src/app.js
@@ -1,1 +1,11 @@
-window.onload=function(){ alert('hello webpack'); }
+require("./css/styles.css");
+
+window.onload=function(){ 
+    //alert('hello webpack'); 
+    var cardNumbre = ['1','2','3','4','5','6','7','8','9','J','Q','K'];
+    var suit = ['Diamonds', 'Hearts', 'Spades','Cluds'];
+    var randomCardNumber = Math.floor(Math.random() * cardNumbre.length);
+    var randomSuitNumber = Math.floor(Math.random() * suit.length);
+    var finalSuit = suit[randomSuitNumber];
+    
+}
